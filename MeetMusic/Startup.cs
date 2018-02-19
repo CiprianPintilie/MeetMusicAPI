18d2e06818773457b04152088b02335da1ab3961
CiprianPintilie <pintilie.c.a@gmail.com>
2018-02-19 16:54:21 +0100

    Change - Remove HTTPS onligation

--- MeetMusic/Startup.cs
+++ MeetMusic/Startup.cs
@@ -19,11 +19,6 @@ namespace MeetMusic
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<MvcOptions>(options =>
-            {
-                options.Filters.Add(new RequireHttpsAttribute());
-            });
-
             services.AddMvc();
         }
 
