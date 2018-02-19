30d9980431f4a78899d6ee1856d3a401bb6e0a2d
CiprianPintilie <pintilie.c.a@gmail.com>
2018-02-19 11:01:19 +0100

    New - Init

---
+++ MeetMusic/Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeetMusic.Controllers
+{
+    [Route("api/[controller]")]
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Get()
+        {
+            return Ok("Hello");
+        }
+
+        [HttpGet]
+        [Route("{name}")]
+        public IActionResult Get(string name)
+        {
+            return Ok($"Hello {name}");
+        }
+    }
+}
---
+++ MeetMusic/Controllers/IndexController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeetMusic.Controllers
+{
+    [Route("api/[controller]")]
+    public class IndexController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return Ok("API started");
+        }
+    }
+}
