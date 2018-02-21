c265574247cf38368f50205a50bf050da11cb6d9
CiprianPintilie <pintilie.c.a@gmail.com>
2018-02-21 11:18:03 +0100

    Change - User model

--- MeetMusic/Controllers/UserController.cs
+++ MeetMusic/Controllers/UserController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using MeetMusic.Interfaces;
+﻿using MeetMusic.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -24,7 +20,6 @@ namespace MeetMusic.Controllers
         public IActionResult GetAllUsers()
         {
             return Ok(_userService.GetAllUsers());
-            //return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
--- MeetMusicModels/Models/User.cs
+++ MeetMusicModels/Models/User.cs
@@ -4,35 +4,42 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MeetMusicModels.Models
 {
+    [Table("user")]
     public class User
     {
         [Key]
+        [Column("id")]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
+        [Column("username")]
         [Required(ErrorMessage = "required")]
         [RegularExpression(@"^[a-zA-Z0-9]{4,20}$",
         ErrorMessage = "Username must be at least 5 characters and not exceed 20 characters")]
         public string Username { get; set; }
 
-
-        [DataType(DataType.EmailAddress)]
-        [Required(ErrorMessage = "required")]
-        [RegularExpression(@"^([a-zA-Z0-9_-.]+)@(([[0-9]{1,3}" +
-                           @".[0-9]{1,3}.[0-9]{1,3}.)|(([a-zA-Z0-9-]+" +
-                           @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(]?)$",
-        ErrorMessage = "Not a mail adress")]
-        public string Email { get; set; }
-        
+        [Column("password")]
         [DataType(DataType.Password)]
         [Required(ErrorMessage = "required")]
         [StringLength(10, MinimumLength = 6, ErrorMessage = "Password must contains between {2} & {1} characters.")]
         public string Password { get; set; }
 
+        [Column("mail")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "required")]
+        [RegularExpression(@"^([a-zA-Z0-9_-.]+)@(([[0-9]{1,3}" +
+                           @".[0-9]{1,3}.[0-9]{1,3}.)|(([a-zA-Z0-9-]+" +
+                           @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(]?)$",
+            ErrorMessage = "Not a mail adress")]
+        public string Email { get; set; }
+
+        [Column("birth_date")]
         public DateTime BirthDate { get; set; }
 
+        [Column("longitude")]
         public string Longitude { get; set; }
 
+        [Column("latitude")]
         public string Latitude { get; set; }
     }
 }
