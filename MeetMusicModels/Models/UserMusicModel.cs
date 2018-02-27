9aab0fcfad0bdbed5a9c57a266d95c099a38fc7b
CiprianPintilie <pintilie.c.a@gmail.com>
2018-02-27 12:37:40 +0100

    Change - Add UserMusicModel model

---
+++ MeetMusicModels/Models/UserMusicModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MeetMusicModels.Models
+{
+    [Table("music_user")]
+    public class UserMusicModel
+    {
+        [Column("user_id")]
+        public int UserId { get; set; }
+
+        [Column("family_id")]
+        public int FamilyId { get; set; }
+
+        [Column("rank")]
+        public int Position { get; set; }
+    }
+}
